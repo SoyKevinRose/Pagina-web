88a52eb0885f1de2db715e1dd439b0fcb9166ec6
Kevin Bejarano <kevincamilo1725@gmail.com>
2021-12-18 09:42:22 -0500

    segunda versión

--- mind.js
+++ mind.js
@@ -1,4 +1,6 @@
 let num1 =prompt('ingrese el primer número');
 let num2 =prompt('ingrese el segundo número');
+let num2 =prompt('ingrese el tercer número');
 let op = num1 + num2;
-document.write(op);
+
+document.write('La suma de los dos números son ',op, ' su resta es: ', Math.abs(num|-num2));
