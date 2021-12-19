07c5f191307ba93f187ed26697378dbf8e2bf3f0
Kevin Bejarano <kevincamilo1725@gmail.com>
2021-12-19 09:10:54 -0500

    Cuarto cambio

--- mind.js
+++ mind.js
@@ -1,6 +1,6 @@
 let num1 =prompt('ingrese el primer número');
 let num2 =prompt('ingrese el segundo número');
-let num2 =prompt('ingrese el tercer número');
+let num3 =prompt('ingrese ');
 let op = num1 + num2;
 
 document.write('La suma de los dos números son ',op, ' su resta es: ', Math.abs(num|-num2));
